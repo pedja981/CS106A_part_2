da51aaff9d8b499ff4806170ac0e38062424bd51
pedja981 <pedja981@users.noreply.github.com>
2016-03-23 17:04:30 +0100

    MyDrawingRandomCircles
    MyDrawingRandomCircles class, for drawing random circles on canvas...

---
+++ Section_handout_3.java
@@ -0,0 +1,61 @@
+/**
+ * Section handout #3, Lecture 10
+ * Program for random drawing of circles across the canvas
+ * @author asus
+ *
+ */
+import acm.program.*; 
+import acm.graphics.*; 
+import acm.util.*;
+import java.awt.*;
+
+public class MyDrawingRandomCircles extends GraphicsProgram {
+	// first - the class variables
+	/* Number of circles to draw */
+	private static final int NUMBER_OF_CIRCLES = 10;
+	/* Minimal radius for the circle, in pixels */
+	private static final double MIN_RADIUS = 5.0;
+	/* Maximal radius for the circle, in pixels */
+	private static final double MAX_RADIUS = 50.0;
+	/* We need to have pause time in order to achieve
+	 * the impression of animation for the human eye */
+	private static final int PAUSE_TIME = 50;
+	
+	
+	public void run() {
+		for (int i=0; i<NUMBER_OF_CIRCLES; i++) {
+			drawRandomCircle();
+		}
+	}
+	
+	
+	
+	
+	/* I have used top-down design - add the methods needed,
+	 * and write them after that */
+	
+	
+	
+	/* Method for drawing the circle with random: 
+	 * fill color, radius, position.
+	 * Condition: Circle must fit into canvas */
+	private void drawRandomCircle() {
+		/* I feel uncomfortable with dealing with the
+		 * first condition - we must try creating the circles,
+		 * for each 10, and keep trying until the condition
+		 * is satisfied. So, I would use the "loop and a half"*/
+		while (true) {
+			setupCircle();
+			if (circleInCanvas()) {
+				addRandomCircle();
+				break;
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+}
